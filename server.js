2ba382113603aa0d018e14f9b003ef7b6032c150
fckolob <131497920+fckolob@users.noreply.github.com>
2025-05-21 21:50:36 -0300

    changes added

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +30,9 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 //Index route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
